3d2f233d34cc9fdd88ff1dd10345aff5a5178a01
Paul Apelt <lt.kempf@gmail.com>
2015-08-28 01:37:22 +1000

    Added owncloud config. Ok, now I am going to sleep. Things to do: switch to pgsql from sqlite, make you guys admins, unfuck nginx config formatting and style

---
+++ Configs/config.php
@@ -0,0 +1,17 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'oc17l0umeajj',
+  'passwordsalt' => 'nlH4S+2sacsqe2b/PGBHCBPVCjSuKG',
+  'secret' => 'UYQj0Aa/jdIw0jdXk7ZcV6SgnnkGs0GCyVehjgzvjqanuf9g',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.5.229',
+    1 => '203.129.33.80',
+  ),
+  'datadirectory' => '/home/owncloud',
+  'overwrite.cli.url' => 'http://192.168.5.229',
+  'dbtype' => 'sqlite3',
+  'version' => '8.1.1.3',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+);
